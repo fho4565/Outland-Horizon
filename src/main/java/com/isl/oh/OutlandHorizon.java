207ba110b3c408d8914c7c125164ee4e43d6a304
fho4565 <qazplmylg@qq.com>
2024-08-20 20:03:58 +0800

    清除错误提交

--- src/main/java/com/isl/oh/OutlandHorizon.java
+++
@@ -1,8 +0,0 @@
-package com.isl.oh;
-
-import net.minecraftforge.fml.common.Mod;
-
-@Mod(Utils.MODID)
-public class OutlandHorizon {
-
-}
--- src/main/java/com/isl/oh/Utils.java
+++
@@ -1,5 +0,0 @@
-package com.isl.oh;
-
-public class Utils {
-    public static final String MODID = "oh";
-}
